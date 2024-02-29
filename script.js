9752a519cf1cff2c79472ea77bb081468ebe0874
Mina <minaschool11@gmail.com>
2024-02-29 13:39:42 +0200

    Made the calculator functional

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
-function add(n1, n2){
-    return n1 + n2;
-}
-function subtract(n1, n2){
-    return n1 - n2;
-}
-function multiply(n1, n2){
-    return n1 * n2;
-}
-function divide(n1, n2){
-    return n1 / n2;
+const display = document.querySelector('.calc-display');
+
+function appendToDisplay(input){
+    display.textContent += input; // Update textContent instead of value
 }
 
-function operate(n1, operator, n2){
-    if(operator === '+')
-        add(n1, n2)
-    if(operator === '-')
-        subtract(n1, n2)
-    if(operator === '*')
-        multiply(n1, n2)
-    if(operator === '/')
-        divide(n1, n2)
+function clearDisplay(){
+    display.textContent = ''; // Update textContent instead of value
+}
+
+function calculate(){
+    try{
+        let result = eval(display.textContent);
+        if (!isFinite(result)) {
+            throw new Error('Cannot divide by zero');
+        }
+        display.textContent = result;
+    }
+    catch(error){
+        display.textContent = 'Error';
+    }
 }
